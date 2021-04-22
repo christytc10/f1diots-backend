ab1afed2885e97f4087a341b147f776dccbd1768
Christopher Campbell <christytc10@gmail.com>
2021-04-22 09:03:12 +0100

    initial commit

---
+++ src/main/java/com/f1diots/racedata/RacedataApplication.java
@@ -0,0 +1,23 @@
+package com.f1diots.racedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RacedataApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacedataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1diots/racedata/RacedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1diots.racedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
