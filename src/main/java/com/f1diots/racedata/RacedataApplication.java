fadf29323c369c5fddcaa6a80f5df15114890c2d
Christopher Campbell <christytc10@gmail.com>
2021-04-22 09:24:56 +0100

    test autodeploy

--- src/main/java/com/f1diots/racedata/RacedataApplication.java
+++ src/main/java/com/f1diots/racedata/RacedataApplication.java
@@ -13,7 +13,7 @@ public class RacedataApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World!";
+		return "Hello New Auto-Deployed World!";
 	}
 
 	public static void main(String[] args) {
